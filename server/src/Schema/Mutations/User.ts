3cc327f9f8822552207dcea55cb5705b1777895b
Borislav Stoyanov <bostoycontact@gmail.com>
2021-12-31 14:47:54 +0200

    create Messages type

--- server/src/Schema/Mutations/User.ts
+++ server/src/Schema/Mutations/User.ts
@@ -1,5 +1,6 @@
 import { GraphQLID, GraphQLString } from 'graphql';
 import { UserType } from '../TypeDefs/User';
+import { MessageType } from '../TypeDefs/Messages';
 import { Users } from '../../Entities/Users';
 import IUser from '../../interfaces/IUser';
 
@@ -17,18 +18,20 @@ export const CREATE_USER = {
 };
 
 export const DELETE_USER = {
-    type: UserType,
+    type: MessageType,
     args: {
         id: { type: GraphQLID }
     },
     async resolve(parent: IUser, args: IUser) {
         const { id } = args;
         await Users.delete({ id });
+
+        return {successful:true, message: 'DELETE SUCCESSFUL ✔'}
     }
 };
 
 export const UPDATE_PASSWORD = {
-    type: UserType,
+    type: MessageType,
     args: {
         username: { type: GraphQLString },
         oldPassword: { type: GraphQLString },
@@ -40,8 +43,13 @@ export const UPDATE_PASSWORD = {
         const user = await Users.findOne({ username });
         const dbPassword = user?.password;
 
+        if (!user) {
+            throw new Error('USERNAME DOESNT EXIST 😥');
+        }
+
         if (oldPassword === dbPassword) {
-            await Users.update({username}, {password: newPassword});
+            await Users.update({ username }, { password: newPassword });
+            return{successful:true, message: 'PASSWORD CHANGED SUCCESSFULLY ✔'}
 
         } else {
             throw new Error('PASSWORDS DO NOT MATCH 😥');
